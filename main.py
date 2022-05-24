bf9795562c9d506c6f5abf91d899e2f8a51f2fd8
Klisman Ramos <klismanrds@gmail.com>
2022-05-24 16:55:14 -0300

    Alteração para apresentação pessoal

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
-print('Hello World')
+print('Primeiro Programa')
 nome = input('Qual o seu nome?!  ')
-print(f'Olá {nome} Prazer em conhecê-lo')
+idade= input('Qual a sua idade?!  ')
+profissao = input('Qual a sua Profissao: ')
+local_trabalho = input('Qual bairro você trabalha?! ')
+
+print(f'''Meu nome é: {nome} 
+Tenho {idade} anos
+E trabalho como {profissao} no bairro {local_trabalho}''')
