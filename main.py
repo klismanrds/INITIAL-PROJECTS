d0b1b06ab38e6f8e05a882c5b4e6b49948d34bc4
Klisman Ramos <85908584+klismanrds@users.noreply.github.com>
2022-07-22 17:06:47 -0300

    Update and rename main.py to calculadora_imc.py
    Programa que calcula imc

--- main.py
+++
@@ -1,9 +0,0 @@
-print('Primeiro Programa')
-nome = input('Qual o seu nome?!  ')
-idade= input('Qual a sua idade?!  ')
-profissao = input('Qual a sua Profissao: ')
-local_trabalho = input('Qual bairro você trabalha?! ')
-
-print(f'''Meu nome é: {nome} 
-Tenho {idade} anos
-E trabalho como {profissao} no bairro {local_trabalho}''')
